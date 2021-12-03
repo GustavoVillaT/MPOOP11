dd6ccd5d7994b4f152cb93f20e1c544c714e31e3
GustavoVilla1 <78132133+GustavoVillaT@users.noreply.github.com>
2021-12-03 01:01:35 +0000

    Initial commit

---
+++ hilos/HiloR.java
@@ -0,0 +1,12 @@
+package hilos;
+
+public class HiloR implements Runnable{
+
+
+  public void run(){
+    for(int i=0;i<10;i++){
+      System.out.println("Iteracion "+i+" del "+Thread.currentThread().getName());
+    }
+    System.out.println("Terminan el hilo "+Thread.currentThread().getName());
+  }
+}
